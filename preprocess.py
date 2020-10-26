0caee39272fea4b8cb8870d3d01e4e6df8ad776f
Jeonghwan Kim <jeonghwankim123@gmail.com>
2020-10-26 13:10:52 +0000

    Add vocab_shop_no to def load_and_cache_examples()

--- preprocess.py
+++ preprocess.py
@@ -6,6 +6,7 @@ import logging
 import re
 import torch
 from torch.utils.data import TensorDataset
+from collections import defaultdict
 
 logger = logging.getLogger(__name__)
 
@@ -14,23 +15,26 @@ class OrderExample(object):
     # A class for a data instance for Baemin Order dataset
     def __init__(
         self,
-        mem_no,
-        dvc_id,
-        shop_no,  # The prime input
-        shop_owner_no,
-        rgn1_cd,
-        rgn2_cd,
-        rgn3_cd,
-        ord_msg,
-        ord_tm,
-        item_name,
-        review_yn,
-        review_created_tm,
-        rating,
-        ord_price,
-        delivery_yn,
-        item_quantity,
-        cpn_use_cnt,
+        mem_no=None,
+        dvc_id=None,
+        shop_no=None,  # The prime input
+        shop_owner_no=None,
+        rgn1_cd=None,
+        rgn2_cd=None,
+        rgn3_cd=None,
+        ord_msg=None,
+        ord_tm=None,
+        item_name=None,
+        review_yn=None,
+        review_created_tm=None,
+        rating=None,
+        ord_price=None,
+        delivery_yn=None,
+        item_quantity=None,
+        cpn_use_cnt=None,
+        ord_dt=None,
+        abuse_yn=None,
+        abuse_type=None,
     ):
         self.mem_no = mem_no
         self.dvc_id = dvc_id
@@ -49,13 +53,16 @@ class OrderExample(object):
         self.delivery_yn = delivery_yn
         self.item_quantity = item_quantity
         self.cpn_use_cnt = cpn_use_cnt
+        self.ord_dt = ord_dt
+        self.abuse_yn = abuse_yn
+        self.abuse_type = abuse_type
 
     def __repr__(self):
-        sample_str = "\nMember_no: {}\nOrder_msg: {}\nRating: {}\nDelivery: {}\nItem Name: {}\n".format(self.mem_no, self.ord_msg, self.rating, self.delivery_yn, self.item_name)
+        sample_str = "\nShop_no: {}\nOrder_dt: {}\n".format(self.shop_no, self.ord_dt)
         return sample_str
     
     def __str__(self):
-        sample_str = "\nMember_no: {}\nOrder_msg: {}\nRating: {}\nDelivery: {}\nItem Name: {}\n".format(self.mem_no, self.ord_msg, self.rating, self.delivery_yn, self.item_name)
+        sample_str = "\nShop_no: {}\nOrder_dt: {}\n".format(self.shop_no, self.ord_dt)
         return sample_str
 
 
@@ -96,15 +103,15 @@ class BaeminProcessor(object):
             df = pd.DataFrame(columns=headers)
             for i, row in enumerate(tqdm(data_reader)):
                 df.loc[i] = row[:24]
-                if i > 500000:  # TODO: set to 1000 instances due to speed limit
+                if i > 1000:  # TODO: set to 1000 instances due to speed limit
                     break
         print("Data Length: ", df.shape[0])
-        # dataset = _create_sample(df)
         print(df.iloc[0])
         return df
 
     def _create_examples(self, input_data, mode):
         examples = []
+        vocab_shop_no = defaultdict(list)
         if mode == "train":
             num_data = input_data.shape[0]
             # Extract a single instance
@@ -116,7 +123,7 @@ class BaeminProcessor(object):
                     example = OrderExample(
                         mem_no=ord_instance['mem_no'],
                         dvc_id=ord_instance['dvc_id'],
-                        shop_no=ord_instance['shop_no'], # prime input
+                        shop_no=ord_instance['shop_no'],  # prime input
                         shop_owner_no=ord_instance['shop_owner_no'],
                         rgn1_cd=ord_instance['rgn1_cd'],
                         rgn2_cd=ord_instance['rgn2_cd'],
@@ -131,18 +138,30 @@ class BaeminProcessor(object):
                         delivery_yn=ord_instance['delivery_yn'],
                         item_quantity=ord_instance['item_quantity'],
                         cpn_use_cnt=ord_instance['cpn_use_cnt'],
+                        ord_dt=ord_instance['ord_dt']
                     )
                     examples.append(example)
+                    # if ord_instance['shop_no'] not in vocab_shop_no:
+                    vocab_shop_no[ord_instance['shop_no']] = None  # Creating a vocab_shop_no
             print("[Data Example List Created.]\n")
             print('Dataset length: ', len(examples))
-        elif mode == "dev":
+            print("Unique shop_no: ", len(vocab_shop_no))
+
+        elif mode == "dev" or mode == "test":
             num_data = input_data.shape[0]
             for i in tqdm(range(num_data), desc="(Dev) Create Baemin Order Samples"):
                 ord_instance = input_data.iloc[i]
-                # example = 
+                example = OrderExample(
+                    shop_no=ord_instance['shop_no'],  # prime input
+                    ord_dt=ord_instance['ord_dt'],
+                    abuse_yn=ord_instance['abuse_yn'],
+                    abuse_type=ord_instance['abuse_type']
+                )
+                examples.append(example)
+                vocab_shop_no[ord_instance['shop_no']] = None
                 # TODO: Implement for validation.csv
 
-        return examples
+        return examples, vocab_shop_no
 
     def get_examples(self, mode):
         """
@@ -175,91 +194,98 @@ def convert_examples_to_features(examples, max_seq_len, tokenizer,
                                  cls_token_segment_id=0,
                                  pad_token_segment_id=0,
                                  sequence_a_segment_id=0,
-                                 mask_padding_with_zero=True):
-    # Setting based on the current model type
-    cls_token = tokenizer.cls_token
-    sep_token = tokenizer.sep_token
-    pad_token_id = tokenizer.pad_token_id
+                                 mask_padding_with_zero=True,
+                                 mode="train"):
+    if mode == "train":
+        # Setting based on the current model type
+        cls_token = tokenizer.cls_token
+        sep_token = tokenizer.sep_token
+        pad_token_id = tokenizer.pad_token_id
 
-    features = []
-    for (ex_index, example) in tqdm(enumerate(examples)):
-        if ex_index % 5000 == 0:
-            logger.info("Writing example %d of %d" % (ex_index, len(examples)))
-        tokens = tokenizer.tokenize(' '.join([example.ord_msg, example.item_name])) # TODO: tokenize textual data
+        features = []
+        for (ex_index, example) in tqdm(enumerate(examples)):
+            if ex_index % 5000 == 0:
+                logger.info("Writing example %d of %d" % (ex_index, len(examples)))
+            tokens = tokenizer.tokenize(' '.join([example.ord_msg, example.item_name])) # TODO: tokenize textual data
 
-        # Account for [CLS] and [SEP]
-        special_tokens_count = 2
-        if len(tokens) > max_seq_len - special_tokens_count:
-            tokens = tokens[:(max_seq_len - special_tokens_count)]
+            # Account for [CLS] and [SEP]
+            special_tokens_count = 2
+            if len(tokens) > max_seq_len - special_tokens_count:
+                tokens = tokens[:(max_seq_len - special_tokens_count)]
 
-        # Add [SEP] token
-        tokens += [sep_token]
-        token_type_ids = [sequence_a_segment_id] * len(tokens)
+            # Add [SEP] token
+            tokens += [sep_token]
+            token_type_ids = [sequence_a_segment_id] * len(tokens)
+            
+            # Add [CLS] token
+            tokens = [cls_token] + tokens
+            cls_token_segment_id = 0
+            token_type_ids = [cls_token_segment_id] + token_type_ids
+            # print("Culprit! >> ", cls_token_segment_id) # TODO: Error: why is cls_token_segment_id == "train"?
 
-        # Add [CLS] token
-        tokens = [cls_token] + tokens
-        token_type_ids = [cls_token_segment_id] + token_type_ids
+            input_ids = tokenizer.convert_tokens_to_ids(tokens)
 
-        input_ids = tokenizer.convert_tokens_to_ids(tokens)
+            # The mask has 1 for real tokens and 0 for padding tokens. Only real
+            # tokens are attended to.
+            attention_mask = [1 if mask_padding_with_zero else 0] * len(input_ids)
 
-        # The mask has 1 for real tokens and 0 for padding tokens. Only real
-        # tokens are attended to.
-        attention_mask = [1 if mask_padding_with_zero else 0] * len(input_ids)
+            # Zero-pad up to the sequence length.
+            padding_length = max_seq_len - len(input_ids)
+            input_ids = input_ids + ([pad_token_id] * padding_length)
+            attention_mask = attention_mask + ([0 if mask_padding_with_zero else 1] * padding_length)
+            token_type_ids = token_type_ids + ([pad_token_segment_id] * padding_length)
 
-        # Zero-pad up to the sequence length.
-        padding_length = max_seq_len - len(input_ids)
-        input_ids = input_ids + ([pad_token_id] * padding_length)
-        attention_mask = attention_mask + ([0 if mask_padding_with_zero else 1] * padding_length)
-        token_type_ids = token_type_ids + ([pad_token_segment_id] * padding_length)
+            assert len(input_ids) == max_seq_len, "Error with input length {} vs {}".format(len(input_ids), max_seq_len)
+            assert len(attention_mask) == max_seq_len, "Error with attention mask length {} vs {}".format(len(attention_mask), max_seq_len)
+            assert len(token_type_ids) == max_seq_len, "Error with token type length {} vs {}".format(len(token_type_ids), max_seq_len)
 
-        assert len(input_ids) == max_seq_len, "Error with input length {} vs {}".format(len(input_ids), max_seq_len)
-        assert len(attention_mask) == max_seq_len, "Error with attention mask length {} vs {}".format(len(attention_mask), max_seq_len)
-        assert len(token_type_ids) == max_seq_len, "Error with token type length {} vs {}".format(len(token_type_ids), max_seq_len)
+            # Non-text input (item_quantity, cpn_use_cnt, ord_price, rating)
+            # TODO: These features need normalization (except for binary features)
+            if example.item_quantity.isnumeric() and example.cpn_use_cnt.isnumeric() and example.ord_price.isnumeric():
+                item_quantity = int(example.item_quantity)
+                cpn_use_cnt = int(example.cpn_use_cnt)
+                ord_price = int(example.ord_price)
+                rating = int(float(example.rating)) if len(example.rating) < 3 else 0
+                nontext_features = [item_quantity, cpn_use_cnt, ord_price, rating]
+            else:
+                continue  # TODO: Case for erroneous instances
 
-        # Non-text input (item_quantity, cpn_use_cnt, ord_price, rating)
-        # TODO: These features need normalization (except for binary features)
-        if example.item_quantity.isnumeric() and example.cpn_use_cnt.isnumeric() and example.ord_price.isnumeric():
-            item_quantity = int(example.item_quantity)
-            cpn_use_cnt = int(example.cpn_use_cnt)
-            ord_price = int(example.ord_price)
-            rating = int(float(example.rating)) if len(example.rating) < 3 else 0
-            nontext_features = [item_quantity, cpn_use_cnt, ord_price, rating]
-        else:
-            continue  # TODO: What to do with erroneous instances?
+            '''
+            1) Everything else is label `0`
+            2) If len(item_name_list) < 2 and ord_price < 10000, then label `1` (order_fraud)
+            3) If review_yn == 1 and (len(ord_msg) < 3 or ord_msg == null), then label `2` (review_fraud)
+            4) If cpn_use_cnt == 0 and ord_price < 10000, then label `3` (coupon fraud)
+            '''
+            label_id = 0
+            item_name_list = re.split(',|\+', example.item_name)
+            if len(item_name_list) < 2 and int(example.ord_price) < 10000:
+                label_id = 1
+            elif int(example.review_yn) == 1 and (len(example.ord_msg) < 3 or example.ord_msg == 'null'):
+                label_id = 2
+            elif int(example.cpn_use_cnt) == 0 and int(example.ord_price) < 10000:
+                label_id = 3
 
-        '''
-        1) Everything else is label `0`
-        2) If len(item_name_list) < 2 and ord_price < 10000, then label `1` (order_fraud)
-        3) If review_yn == 1 and (len(ord_msg) < 3 or ord_msg == null), then label `2` (review_fraud)
-        4) If cpn_use_cnt == 0 and ord_price < 10000, then label `3` (coupon fraud)
-        '''
-        label_id = 0
-        item_name_list = re.split(',|\+', example.item_name)
-        if len(item_name_list) < 2 and int(example.ord_price) < 10000:
-            label_id = 1
-        elif int(example.review_yn) == 1 and (len(example.ord_msg) < 3 or example.ord_msg == 'null'):
-            label_id = 2
-        elif int(example.cpn_use_cnt) == 0 and int(example.ord_price) < 10000:
-            label_id = 3
+            if ex_index < 5:
+                logger.info("*** Example ***")
+                logger.info("shop_no: %s" % example.shop_no)
+                logger.info("tokens: %s" % " ".join([str(x) for x in tokens]))
+                logger.info("input_ids: %s" % " ".join([str(x) for x in input_ids]))
+                logger.info("attention_mask: %s" % " ".join([str(x) for x in attention_mask]))
+                logger.info("token_type_ids: %s" % " ".join([str(x) for x in token_type_ids]))
+                logger.info("label: %s" % (label_id))
 
-        if ex_index < 5:
-            logger.info("*** Example ***")
-            logger.info("shop_no: %s" % example.shop_no)
-            logger.info("tokens: %s" % " ".join([str(x) for x in tokens]))
-            logger.info("input_ids: %s" % " ".join([str(x) for x in input_ids]))
-            logger.info("attention_mask: %s" % " ".join([str(x) for x in attention_mask]))
-            logger.info("token_type_ids: %s" % " ".join([str(x) for x in token_type_ids]))
-            logger.info("label: %s" % (label_id))
+            features.append(
+                OrderFeatures(shop_no=example.shop_no,
+                              input_ids=input_ids,
+                              nontext_features=nontext_features,
+                              attention_mask=attention_mask,
+                              token_type_ids=token_type_ids,
+                              label_id=label_id
+                            ))
 
-        features.append(
-            OrderFeatures(shop_no=example.shop_no,
-                          input_ids=input_ids,
-                          nontext_features=nontext_features,
-                          attention_mask=attention_mask,
-                          token_type_ids=token_type_ids,
-                          label_id=label_id
-                          ))
-    
+    elif mode == "dev" or "test":
+        pass
+
     return features
 
 
@@ -271,37 +297,41 @@ def load_and_cache_examples(args, tokenizer, mode):
     cached_file_name = 'cached_{}_{}_{}_{}'.format(
         args.task, list(filter(None, args.model_name_or_path.split("/"))).pop(), args.max_seq_len, mode)
 
+    cached_vocabs_name = 'cached_{}_{}_{}_{}'.format(
+        args.task, list(filter(None, args.model_name_or_path.split("/"))).pop(), "vocabs", mode)
+
     cached_features_file = os.path.join(args.data_dir, cached_file_name)
-    if os.path.exists(cached_features_file):
+    cached_vocabs_file = os.path.join(args.data_dir, cached_vocabs_name)
+    if os.path.exists(cached_features_file) and os.path.exists(cached_vocabs_file):
         logger.info("Loading features from cached file %s", cached_features_file)
         features = torch.load(cached_features_file)
+        vocab_shop_no = torch.load(cached_vocabs_file)
     else:
         logger.info("Creating features from dataset file at %s", args.data_dir)
         if mode == "train":
-            examples = processor.get_examples("train")
+            examples, vocab_shop_no = processor.get_examples("train")
         elif mode == "dev":
-            examples = processor.get_examples("dev")
+            examples, vocab_shop_no = processor.get_examples("dev")
         elif mode == "test":
-            examples = processor.get_examples("test")
+            examples, vocab_shop_no = processor.get_examples("test")
         else:
             raise Exception("ModeError: Only train, dev and test modes are available")
 
-        features = convert_examples_to_features(examples, args.max_seq_len, tokenizer)
+        features = convert_examples_to_features(examples, args.max_seq_len, tokenizer, mode)
         logger.info("Saving features into cached file %s", cached_features_file)
         torch.save(features, cached_features_file)
 
     # Convert to Tensors and build dataset
-    all_shop_no = torch.tensor([f.shop_no for f in features], dtype=str)
     all_input_ids = torch.tensor([f.input_ids for f in features], dtype=torch.long)
-    all_nontext_features = torch.tensor([f.input_ids for f in features], dtype=torch.float)
+    all_nontext_features = torch.tensor([f.nontext_features for f in features], dtype=torch.float)
     all_attention_mask = torch.tensor([f.attention_mask for f in features], dtype=torch.long)
     all_token_type_ids = torch.tensor([f.token_type_ids for f in features], dtype=torch.long)
     all_label_ids = torch.tensor([f.label_id for f in features], dtype=torch.long)
     print('all_input_ids: ', all_input_ids.shape)
     print('all_label_ids: ', all_label_ids.shape)
 
-    dataset = TensorDataset(all_shop_no, all_input_ids, all_nontext_features, all_attention_mask,
+    dataset = TensorDataset(all_input_ids, all_nontext_features, all_attention_mask,
                             all_token_type_ids, all_label_ids)
 
     print("Dataset Preprocessing Complete!")
-    return dataset
+    return dataset, vocab_shop_no
