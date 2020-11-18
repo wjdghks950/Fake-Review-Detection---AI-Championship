021280f612c1d8c8e69cca60b76c1451a22f2fc5
Jeonghwan Kim <jeonghwankim123@gmail.com>
2020-11-18 15:37:56 +0000

    Training script updated

--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-python main.py --train_file abuse_train.csv --logging_steps 50 $1 $2
+python main.py --train_file abuse_train.csv --dev_file abuse_dev.csv --logging_steps 10 --save_steps 200 --model_type kobert $1 $2
